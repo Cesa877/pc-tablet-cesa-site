5d41b2f8db400a823ae201980cfc4fa5b3b8ac36
Cesa877 <boy.twitch20@gmail.com>
2025-01-24 14:32:44 +0100

    Update 1
    Aggiunta pagina "app" e cambio font pagina "Build PC"

---
+++ script-app.js
@@ -0,0 +1,37 @@
+const toggleSwitch = document.getElementById('switch');
+const themeLink = document.getElementById('theme-link');
+
+function applyTheme(theme) {
+    if (theme === 'light') {
+        themeLink.setAttribute('href', 'style-app-bianco.css');
+        toggleSwitch.checked = true;
+    } else {
+        themeLink.setAttribute('href', 'style-app-nero.css');
+        toggleSwitch.checked = false;
+    }
+}
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    applyTheme(savedTheme);
+}
+
+toggleSwitch.addEventListener('change', () => {
+    if (toggleSwitch.checked) {
+        applyTheme('light');
+        localStorage.setItem('theme', 'light');
+    } else {
+        applyTheme('dark');
+        localStorage.setItem('theme', 'dark');
+    }
+});
+
+document.getElementById('download-button').addEventListener('click', function() {
+    console.log('Download button clicked');
+
+    // Mostra l'alert direttamente
+    alert('Grazie per aver scaricato la nostra applicazione. Il file è sicuro e non contiene virus o malware.\nConsigli Utili:\n- Sorgenti Affidabili: Scarica il file solo dal nostro sito ufficiale.\n- Verifica Antivirus: Scansiona il file con il tuo antivirus per ulteriore sicurezza.');
+    
+    // Fai partire il download del file .exe
+    window.location.href = 'app/Convertitore immagini.exe';
+});
