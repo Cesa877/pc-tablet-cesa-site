4d08fd48b9f1cd0171f5527f93b5dfcd72409518
Cesa877 <boy.twitch20@gmail.com>
2025-02-09 20:36:20 +0100

    Update 6
    Fix errore download app image converter

--- script-app.js
+++ script-app.js
@@ -31,14 +31,4 @@ document.getElementById('download-button').addEventListener('click', function() 
 
     // Mostra l'alert direttamente
     alert('Grazie per aver scaricato la nostra applicazione. Il file è sicuro e non contiene virus o malware.\nConsigli Utili:\n- Sorgenti Affidabili: Scarica il file solo dal nostro sito ufficiale.\n- Verifica Antivirus: Scansiona il file con il tuo antivirus per ulteriore sicurezza.');
-    
-    // Fai partire il download del file .exe
-    const fileUrl = 'https://drive.google.com/uc?export=download&id=1PWDDpSh-NfZWvXT57p1Q7ech26o9bqst';
-    const link = document.createElement('a');
-    link.href = fileUrl;
-    link.target = '_blank';
-    link.download = '';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
 });
